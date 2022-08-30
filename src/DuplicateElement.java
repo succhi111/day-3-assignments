c2bf1cb5ef9547f829fdc7aef04dc29f0b0c8f70
succhi111 <suchendramishra396@gmail.com>
2022-08-30 10:50:31 +0530

    Program to print the duplicate elements of an array

---
+++ src/DuplicateElement.java
@@ -0,0 +1,14 @@
+public class DuplicateElement {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 6, 2, 3, 4, 5, 9, 6};
+
+        for (int i = 0; i < arr.length; i++)
+
+            for (int j = i + 1; j < arr.length; j++)
+
+                if ((arr[i] == arr[j]) && (i != j))
+                    System.out.println(arr[j] + " ");
+                }
+            }
+
+
