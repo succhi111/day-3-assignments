1c4734ccdaec2d1f9df9a64895f876f6ffd0678f
succhi111 <suchendramishra396@gmail.com>
2022-08-30 10:40:58 +0530

    Program to print the elements of an array present on an even position

---
+++ src/EvenPosition.java
@@ -0,0 +1,11 @@
+public class EvenPosition {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        for (int i = 1; i < arr.length; i++)
+            if (i % 2 == 0)
+                System.out.println(arr[i]);
+    }
+}
+
+
